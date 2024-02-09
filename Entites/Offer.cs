e9a4a4368588ff0eebc5276c2534d72d64b5dfea
Denis Lapa <lapa.denis.pro@gmail.com>
2024-02-09 10:22:45 +0100

    Create new Entity Offer.cs

---
+++ Entites/Offer.cs
@@ -0,0 +1,17 @@
+﻿namespace ProjetsJo.Entites
+{
+    public class Offer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public Offer(int id, string name, int quantity, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Quantity = quantity;
+            Price = price;
+        }
+    }
+}
