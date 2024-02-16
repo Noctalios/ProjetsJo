1d66dfa5b5a4c67b58273c8fe058b3f7c1691939
Denis Lapa <lapa.denis.pro@gmail.com>
2024-02-16 10:14:51 +0100

    Modify Roles

---
+++ Entites/Role.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjetsJo.Entities
+{
+    public class Role
+    {
+        public int Id { get; set; }
+
+        public string Label { get; set; }
+
+        public Role(int id, string label) 
+        {
+            Id = id;
+            Label = label;
+        }
+    }
+}
---
+++ Entites/User.cs
@@ -0,0 +1,33 @@
+﻿using ProjetsJo.Entities;
+
+namespace ProjetsJo.Entites
+{
+    public class User
+    {
+        public Guid AccountKey { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public Role Role { get; set; }
+        public List<string>? Tickets { get; set; }
+
+        public User() { }
+        public User(string userName, string lastName, string email, string password, Guid accountKey, Role role)
+        {
+            UserName = userName;
+            Email = email;
+            Password = password;
+            AccountKey = accountKey;
+            Role = role;
+        }
+        public User( string userName, string lastName, string email, string password, Guid accountKey, Role role, List<string>? tickets)
+        {
+            UserName = userName;
+            Email = email;
+            Password = password;
+            AccountKey = accountKey;
+            Role = role;
+            Tickets = tickets;
+        }
+    }
+}
