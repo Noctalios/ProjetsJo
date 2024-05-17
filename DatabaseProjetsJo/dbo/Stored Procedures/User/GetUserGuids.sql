2c22c582459bf2dd1ef4d56ebb9babb3d954b445
Denis Lapa <lapa.denis.pro@gmail.com>
2024-05-17 16:55:37 +0200

    add folders  and getUserGuids.

---
+++ DatabaseProjetsJo/dbo/Stored Procedures/User/GetUserGuids.sql
@@ -0,0 +1,17 @@
+﻿-- =============================================
+-- Description:	<GET User Info on connexion>
+-- =============================================
+
+CREATE PROCEDURE [dbo].[GetUserGuids]
+AS
+  BEGIN TRY
+	BEGIN TRAN GetUserGuids
+
+		SELECT U.[AccountKey]
+		FROM [User] U
+
+	COMMIT TRAN GetUserGuids
+  END TRY
+  BEGIN CATCH
+	ROLLBACK TRAN GetUserGuids
+  END CATCH
