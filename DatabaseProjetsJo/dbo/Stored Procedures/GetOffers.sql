5394314a4f31b32695d99d0875c2585011845997
Denis Lapa <lapa.denis.pro@gmail.com>
2024-05-17 12:23:02 +0200

    correct getOffers

--- DatabaseProjetsJo/dbo/Stored Procedures/GetOffers.sql
+++ DatabaseProjetsJo/dbo/Stored Procedures/GetOffers.sql
@@ -4,22 +4,29 @@
 
 CREATE PROCEDURE [dbo].[GetOffers]
 AS
-  BEGIN TRY
-	BEGIN TRAN GetOffers
+	BEGIN TRY
+		BEGIN TRAN GetOffers
 
-		SELECT
-			O.[Id],
-			O.[Label],
-			O.[Price],
-			O.[TicketNumber],
-			O.[State],
-			COUNT(T.[Id]) AS [Total]
-		FROM Offer O
-		LEFT JOIN (SELECT * FROM Ticket) T 
-			ON T.OfferId = O.Id  
+			SELECT
+				O.[Id],
+				O.[Label],
+				O.[Price],
+				O.[TicketNumber],
+				O.[State],
+				T.Total
+			FROM Offer O
+			LEFT JOIN 
+			(
+				SELECT 
+					T.OfferId,
+					COUNT(T.OfferId) AS [Total]
+				FROM Ticket T
+				GROUP BY OfferId
+			) AS T 
+			ON O.Id = T.OfferId
 	
-	COMMIT TRAN GetOffers
-  END TRY
-  BEGIN CATCH
-	ROLLBACK TRAN GetOffers
-  END CATCH
+		COMMIT TRAN GetOffers
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRAN GetOffers
+	END CATCH
