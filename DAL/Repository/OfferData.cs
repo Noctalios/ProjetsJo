d7e368b7db4a1a8e5e60b18c5868fc46d5b51aaf
Denis Lapa <lapa.denis.pro@gmail.com>
2024-05-30 23:52:23 +0200

    Writing comments OfferData.

--- DAL/Repository/OfferData.cs
+++ DAL/Repository/OfferData.cs
@@ -14,11 +14,19 @@ namespace ProjetsJo.DAL.Repository
         {
             _configuration = configuration;
         }
+        /// <summary>
+        /// Provide the connection string from the Configuration.
+        /// </summary>
+        /// <returns>Return the value of DefaultConnection in appsettings.json.</returns>
         private string GetConnexionString()
         {
             return _configuration.GetConnectionString("DefaultConnection");
         }
 
+        /// <summary>
+        /// Request the Database to retrieve all the offers saved in.
+        /// </summary>
+        /// <returns>Returns all the offers.</returns>
         public List<Offer> GetOffers()
         {
             string sql = "EXEC [GetOffers]";
@@ -51,6 +59,11 @@ namespace ProjetsJo.DAL.Repository
             return offers;
         }
 
+        /// <summary>
+        /// Request the Database to execute the stored procedure CreateOffer.
+        /// The parameters of the stored procedure are the Label, TicketNumber and the price of the newOffer.
+        /// </summary>
+        /// <param name="newOffer">Offer to save in database.</param>
         public void AddOffer(Offer newOffer)
         {
             string sql = "EXEC [CreateOffer] @Label, @TicketNumber, @Price";
@@ -68,6 +81,11 @@ namespace ProjetsJo.DAL.Repository
             }
         }
 
+        /// <summary>
+        /// Request the Database to execute the stored procedure DisableOffer according Id.
+        /// This procedure update the state of the offer to false.
+        /// </summary>
+        /// <param name="id">Id of the offer to disable.</param>
         public void DisableOffer(int id)
         {
             string sql = "EXEC [DisableOffer] @Id";
@@ -83,6 +101,11 @@ namespace ProjetsJo.DAL.Repository
             }
         }
 
+        /// <summary>
+        /// Request the Database to execute the stored procedure UdpdateOffer.
+        /// This procedure can update the Label and the TicketNumber of the offer according Id.
+        /// </summary>
+        /// <param name="OfferToUpdate">Offer to update in database.</param>
         public void UpdateOffer(Offer OfferToUpdate)
         {
             string sql = "EXEC [UpdateOffer] @OfferId, @Label, @TicketNumber";
