232851f2791877463a52dc087e015b31a10e7f81
Denis Lapa <lapa.denis.pro@gmail.com>
2024-05-30 22:47:36 +0200

    Writing comments OfferService.

--- BLL/Services/OfferService.cs
+++ BLL/Services/OfferService.cs
@@ -13,18 +13,34 @@ namespace ProjetsJo.BLL.Services
             _offerData = offerData;
         }
 
+        /// <summary>
+        /// This method permits to retrieve all the offers of the database.
+        /// </summary>
+        /// <returns></returns>
         public List<Offer> GetOffers() => _offerData.GetOffers();
 
+        /// <summary>
+        /// This method requires the DAL to add this new the offer.
+        /// </summary>
+        /// <param name="newOffer">Offer to add</param>
         public void AddOffer(Offer newOffer) 
         {
             _offerData.AddOffer(newOffer);
         }
 
+        /// <summary>
+        /// This method requires the DAL to update the offer.
+        /// </summary>
+        /// <param name="offerToUpdate">Offer to update</param>
         public void UpdateOffer(Offer offerToUpdate)
         {
             _offerData.UpdateOffer(offerToUpdate);
         }
 
+        /// <summary>
+        /// This method requires the DAL to disable according his Id.
+        /// </summary>
+        /// <param name="offerId">Id of the offer to disable</param>
         public void DisableOffer(int offerId)
         {
             _offerData.DisableOffer(offerId);
